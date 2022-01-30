2c0dec277def0cb324d7328b8871997ed07d85ca
cloudsongcast <97848707+cloudsongcast@users.noreply.github.com>
2022-01-30 04:15:34 -0600

    Update stationOne.js

--- js/stationOne.js
+++ js/stationOne.js
@@ -1,15 +1,23 @@
+const noHdrbtn = document.querySelectorAll('header')
+const noFtr = document.querySelectorAll('footer')
+
+	noHdrbtn[0].classList.add('behind')
+    noFtr[0].classList.add('behind')
+
 window.onload = ()=>{
-	setTimeout(function() {
-		
-	}, 3000);
+
 	const btnClkA = document.querySelectorAll('.btn')
-    const vueForm = document.querySelectorAll('.vue-form div div ul li')
-    	vueForm[1].classList.add('behind')
-    	vueForm[2].classList.add('behind')
-    	vueForm[3].classList.add('behind')
-    	console.log(vueForm[1]);
+	
+	
+    const cardGet = document.querySelectorAll('.card')
+        cardGet[0].classList.add('behind')
+    	cardGet[5].classList.add('behind')
+    	cardGet[6].classList.add('behind')
+    	cardGet[4].classList.add('behind')
+    
+    	console.log(cardGet)
     btnClkA[0].addEventListener('click',()=>{
-    	console.log("click click")
+    	
     	
     })
 }
